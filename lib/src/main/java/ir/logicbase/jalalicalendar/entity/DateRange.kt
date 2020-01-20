8b39300241aacc26d42a5fc91646307b2742af8f
Mahdi <m.javaherisaber@gmail.com>
2020-01-21 01:17:00 +0330

    Add ClockPeriod and DateRange entities

---
+++ lib/src/main/java/ir/logicbase/jalalicalendar/entity/DateRange.kt
@@ -0,0 +1,5 @@
+package ir.logicbase.jalalicalendar.entity
+
+import ir.logicbase.jalalicalendar.JalaliCalendar
+
+data class DateRange(var start: JalaliCalendar, var selected: JalaliCalendar, var end: JalaliCalendar)
